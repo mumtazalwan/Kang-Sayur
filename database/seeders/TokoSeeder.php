59cad0b889d6b83c0d79ef251547046a15f569bf
mumtazalwan <mumtazalwan@gmail.com>
2023-06-05 12:06:57 +0700

    [AAM] create seller personal information

--- database/seeders/TokoSeeder.php
+++ database/seeders/TokoSeeder.php
@@ -25,6 +25,7 @@ class TokoSeeder extends Seeder
 
         Toko::create([
             'nama_toko' => 'Toko Bebek Pak Kwan',
+            'img_profile' => '/images/store/kwan.jpg',
             'deskripsi' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industr\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
             'seller_id' => 2,
             'alamat' => 'Jl Jawa XV Bl A/2 Kawasan Industri Cakung,Rorotanu',
