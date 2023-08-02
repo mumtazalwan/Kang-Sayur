f4b07c1cc4dd46f022992d46d8d39e3a2bf9760e
mumtazalwan <mumtazalwan@gmail.com>
2023-08-02 17:36:14 +0700

    [AAM] create history delivery driver

--- database/seeders/KategoriSeeder.php
+++ database/seeders/KategoriSeeder.php
@@ -14,26 +14,32 @@ class KategoriSeeder extends Seeder
     public function run(): void
     {
         Kategori::create([
+            'kategori_img' => 'beras',
             'nama_kategori' => 'Bahan Pokok'
         ]);
 
         Kategori::create([
+            'kategori_img' => 'sayur',
             'nama_kategori' => 'Sayuran'
         ]);
 
         Kategori::create([
+            'kategori_img' => 'buah',
             'nama_kategori' => 'Buah-buahan'
         ]);
 
         Kategori::create([
+            'kategori_img' => 'daging',
             'nama_kategori' => 'Daging'
         ]);
 
         Kategori::create([
+            'kategori_img' => 'unggas',
             'nama_kategori' => 'Unggas'
         ]);
 
         Kategori::create([
+            'kategori_img' => 'telur',
             'nama_kategori' => 'Telur'
         ]);
     }
