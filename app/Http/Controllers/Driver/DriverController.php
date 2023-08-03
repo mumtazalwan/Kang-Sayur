30e7b99a0b252b0d1c54fd3ce521ba8f801fe07d
mumtazalwan <mumtazalwan@gmail.com>
2023-08-03 07:32:10 +0700

    [AAM] minor update

--- app/Http/Controllers/Driver/DriverController.php
+++ app/Http/Controllers/Driver/DriverController.php
@@ -15,7 +15,7 @@ class DriverController extends Controller
 
         $driver = User::join('kendaraans', 'kendaraans.driver_id', '=', 'users.id')
             ->where('kendaraans.driver_id', $user->id)
-            ->select('users.id as driver_id','users.name', 'users.phone_number', 'kendaraans.jenis_kendaraan', 'kendaraans.nomor_polisi')
+            ->select('users.id as driver_id','users.photo','users.name', 'users.phone_number', 'kendaraans.jenis_kendaraan', 'kendaraans.nomor_polisi')
             ->first();
 
         return response()->json([
