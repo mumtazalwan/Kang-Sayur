a7b840a6463e024ad5370d8f2ab827c2cbda4dc2
mumtazalwan <mumtazalwan@gmail.com>
2023-08-09 08:16:08 +0700

    [AAM] manage response in list driver

--- database/migrations/2023_07_25_132835_create_kendaraans_table.php
+++ database/migrations/2023_07_25_132835_create_kendaraans_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -17,6 +16,7 @@ return new class extends Migration
             $table->integer('toko_id');
             $table->bigInteger('noTelfon_cadangan')->nullable();
             $table->enum('jenis_kendaraan', ['Kendaraan bermotor (Roda 2)', 'Mobil boks (pikap)', 'Truk Engkel', 'Truk Pendingin'])->default('Kendaraan bermotor (Roda 2)');
+            $table->string('nama_kendaraan');
             $table->string('nomor_polisi');
             $table->string('nomor_rangka');
             $table->string('photo_ktp')->nullable();
